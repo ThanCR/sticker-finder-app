514e1d386feee51b64a8d257fbc97be282e84fff
Jonnathan <jonzxz12@gmail.com>
2023-05-10 12:55:07 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+
+
+/* animations and styling */
+var searchContainer = document.querySelector('.search-container');
+var searchIcon = document.querySelector('.search-button');
+var searchInput = document.querySelector('.search-box');
+var currentFocus = false;
+
+searchContainer.addEventListener('mouseover', () => {
+    searchContainer.style.backgroundColor = 'var(--darkPrimary)'
+    searchContainer.style.border = '2px solid var(--accent)'
+    searchIcon.style.color = 'var(--accent)'
+    searchInput.style.color = 'var(--accent)'
+})
+
+searchContainer.addEventListener('mouseout', (event) => {
+    if (currentFocus) {
+
+    } else {
+        searchContainer.style.backgroundColor = 'var(--accent)'
+        searchIcon.style.color = 'var(--darkPrimary)'
+        searchInput.style.color = 'var(--darkPrimary)'
+    }
+})
+
+searchInput.addEventListener('focus', () => {
+    currentFocus = true;
+    searchInput.style.caretColor = 'var(--accent)'
+    searchInput.style.color = 'var(--accent)'
+    searchContainer.style.backgroundColor = 'var(--darkPrimary) !important'
+})
+
+searchInput.addEventListener('focusout', () => {
+    currentFocus = false;
+    searchInput.style.caretColor = 'var(--black)'
+    searchIcon.style.color = 'var(--darkPrimary)'
+    searchInput.style.color = 'var(--darkPrimary)'
+    searchContainer.style.backgroundColor = 'var(--accent)'
+})
+
+searchInput.addEventListener('keydown', ({key}) =>{
+    if(key == 'Enter'){
+        searchSticker();
+    }
+
+})
+
+resultsContainer = document.querySelector('.results-container');
+
+const openPanel = () => {
+    resultsContainer.style.height = '25rem'
+    resultsContainer.style.width = 'min(90%, 80rem)'
+}
+
+const closePanel = () => {
+    resultsContainer.style.height = '0'
+    resultsContainer.style.width = '0'
+}
+
+/*  fetching data */
+
+stickerList = '';
+
+
+const searchSticker = () => {
+    closePanel();
+
+    const apikey = 'wcjXOiCheW5TGPktU6TF6Cj8q87TcwWj';
+    const limit = 48;
+    var searchQuery = document.querySelector('.search-box').value;
+    const apiURL = `https://api.giphy.com/v1/stickers/search?api_key=${apikey}&q=${searchQuery}&limit=${limit}`;
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => buildStickerObjects(data))
+        .catch(error => console.log(error))
+
+
+    setTimeout(() => {
+        openPanel();
+    }, 1000);
+
+}
+
+const buildStickerObjects = ({ data }) => {
+    stickerList = '';
+    resultsList = document.querySelector('.results-list')
+    data.forEach(({images}) => {
+            stickerList += `
+            <li class="list-item"><img src="${images.original.url}" loading="lazy" width="100" height="75" frameBorder="0" class="giphy-embed"/></li> 
+            `;
+        });
+        
+    resultsList.innerHTML = stickerList;
+}
+
+
